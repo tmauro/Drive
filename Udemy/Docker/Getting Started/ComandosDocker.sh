153f795ed93a1c9deb3738d6b1655fa701c83004
Mauricio Torres <mauricio.torres.ma@outlook.com>
2018-12-19 01:07:48 -0500

    Nuevos comandos

--- Udemy/Docker/Getting Started/ComandosDocker.sh
+++ Udemy/Docker/Getting Started/ComandosDocker.sh
@@ -1,1 +1,12 @@
 #Comandos Docker
+docker search centos
+docker pull hello-world
+docker images
+docker run hello-world
+docker run 4ab4c602aa5e
+docker pull centos
+docker pull centos:centos6
+docker pull nginx
+
+docker 
+
